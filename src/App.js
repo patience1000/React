47a62f745d3d3476345633713a1c344560e9fdf8
patience1000 <paulpatience577@gmail.com>
2024-01-07 20:36:36 +0100

    Writing code without JSX

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Sidenav from './components/Sidenav';
 import Welcome from './components/welcome';
+import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
@@ -9,10 +10,10 @@ function App() {
         <Navbar name="Patience"/>
         <Sidenav/>
         <Welcome/>
+        <Hello/>
       </div>
        
     </div>
   );
 }
-
 export default App;
