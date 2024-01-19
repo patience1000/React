5ac175229f834c895f817635fc844449622d1790
patience1000 <paulpatience577@gmail.com>
2024-01-19 21:26:51 +0100

    element approach

--- src/Usergreeting.js
+++ src/Usergreeting.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 
-class Usergreeting extends Component{
-    constructor(props){
-        super(props)
 
-        this.state = {
-            isLoggedIn: false
-        }
-    }
 
     render(){
         // return  this.state.isLoggedIn && <div>Welcome Patience</div>
