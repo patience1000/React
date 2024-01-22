710d1a9b2b1c5ce4000c43b85881b2c83d2a7471
patience1000 <paulpatience577@gmail.com>
2024-01-22 17:15:13 +0100

    list-map

--- src/components/List.js
+++ src/components/List.js
@@ -3,13 +3,12 @@ import React from "react";
 function List(){
   
     const names = ['Ada','Obinna','chizoba']
-//    const map1 = names.map(x => x*2)
+
     return(
         <div>
-            <h3>{names[0]}</h3>
-            <h3>{names[1]}</h3>
-            <h3>{names[2]}</h3>
-            <h3>{names[3]}</h3>
+            {
+                names.map(name => <h2>{name}</h2>)
+            }
         </div>
     )
     
