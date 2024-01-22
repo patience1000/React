a9b7df1870468978cac066eb8f3b8154ed2067ae
patience1000 <paulpatience577@gmail.com>
2024-01-22 17:33:17 +0100

    list-map

--- src/components/List.js
+++ src/components/List.js
@@ -2,13 +2,30 @@ import React from "react";
 
 function List(){
   
-    const names = ['Ada','Obinna','chizoba']
-
+    const persons = [
+        {
+            id: 1,
+            name: 'Bruce',
+            age: '30',
+            skill: 'React'
+        },
+    {
+        id: 2,
+        name: 'Clark',
+        age: '25',
+        skill: 'Angular'
+    },
+    {
+        id: 3,
+        name: 'Preye',
+        age: '15',
+        skill: 'Vue'
+    }
+]
+    const personList = persons.map(person => <h2>I am {person.name}, I am {person.age} years old and I am a {person.skill} expert</h2>)
     return(
         <div>
-            {
-                names.map(name => <h2>{name}</h2>)
-            }
+            { personList}
         </div>
     )
     
