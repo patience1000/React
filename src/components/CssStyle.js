34c96a3450e1b220c77ea686a55a8f3154cd362b
patience1000 <paulpatience577@gmail.com>
2024-02-07 23:12:19 +0100

    k

--- src/components/CssStyle.js
+++ src/components/CssStyle.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-function CssStyle(){
-    const name = "John"
-    const isNameShowing = false;
+// function CssStyle(){
+//     const name = "John"
+//     const isNameShowing = false;
+//     return(
+//         <div>
+//             {isNameShowing ? (
+//                 <>
+//                     {name}
+//                 </>
+//             ):(
+//                 <h2>There is no name</h2>
+//             )}
+//         </div>
+       
+//     )
+// }
+const CssStyle = () => {
+    const [counter, setCounter] = useState(0);
     return(
-        <div>
-            {isNameShowing ? (
-                <>
-                    {name}
-                </>
-            ):(
-                <h2>There is no name</h2>
-            )}
+        <div className="App">
+        <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter}>+</button>
         </div>
+        
     )
 }
 export default CssStyle
