8520710fbcdc6a2d14fee8e6dd74ba94930c5cdb
patience1000 <paulpatience577@gmail.com>
2024-02-24 22:00:08 +0100

    Learning form

--- src/components/ShowAnimal.js
+++ src/components/ShowAnimal.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 function showAnimal(){
+
+    const dets = document.getElementById('details')
+    const btn = document.getElementById('btn1')
+    const txt = document.getElementById('text')
     return(
         <div>
-            <div className='form'>
+            <div className='form' id='details'>
                 <form>
                     <label> First Name
                         <input type='Text' name=''/>
@@ -19,7 +23,12 @@ function showAnimal(){
                         <input type="password" placeholder="Enter Password" name="psw" required/>
                     </label>
                 </form>
+                <button id = 'btn1'>Click me</button>
+                <p id='text' ></p>
             </div>
+
+
+
         </div>
     )
 }
