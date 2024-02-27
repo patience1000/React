50fc0f22376b46491d160ae0917c5022613e830f
patience1000 <paulpatience577@gmail.com>
2024-02-28 00:21:44 +0100

    Learning useRef

--- src/components/LearnUseRef.js
+++ src/components/LearnUseRef.js
@@ -1,17 +1,23 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import React from "react";
 
  export default function LearnUseRef (){
     const [name, setName] = useState("")
+    const renderCount = useRef(0)
 
+    useEffect(() => {
+        renderCount.current = renderCount.current + 1
+        })
 
     return(
-        <div>
+        <>
             <h1>Input your Name</h1>
-            
+
             <input value={name} onChange={e => setName(e.target.value) } />
 
             <p>my name is {name}</p>
-        </div>
+
+            I rendered {renderCount} times
+        </>
     )
 }
