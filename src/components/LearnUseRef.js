b0cfca4eec6f26adf450707d43f365416bac8494
patience1000 <paulpatience577@gmail.com>
2024-02-27 14:17:00 +0100

    Learning form

---
+++ src/components/LearnUseRef.js
@@ -0,0 +1,16 @@
+import { useRef, useState } from "react";
+import React from "react";
+
+ export default function LearnUseRef (){
+    const [name, setName] = useState("")
+
+
+    return(
+        <div>
+            <h1>Input your Name</h1>
+            <input type="{name}" onChange={ e => setName( e.target.value)} />
+
+            <p>my name is {name}</p>
+        </div>
+    )
+}
