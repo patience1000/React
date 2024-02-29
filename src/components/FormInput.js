7f61a3e5f2cb8f44cf6e1876963920c0bd0d6b3f
patience1000 <paulpatience577@gmail.com>
2024-02-29 23:16:55 +0100

    form feedback

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -4,11 +4,22 @@ import React, { useState } from "react";
 function FormInput(){
     const [name, setName] = useState();
     const [score, setScore] = useState(10);
+    const [comment, setComment] = useState("");
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         setName("");
         alert('Form submitted successfully')
+    }
+    const actionSubmit = (e) =>{
+        e.preventDefault();
+        if (Number(score) <= 5 && comment.length <=10){
+            alert("Please state why the service was poor");
+            return
+        }
+        alert('Thanks for your feedback')
+        setComment("");
+        setScore(10);
     }
     return(
         <div>
@@ -25,7 +36,7 @@ function FormInput(){
                 </fieldset>
             </form>
             <br/>
-            <form >
+            <form onSubmit={actionSubmit}>
                 <fieldset>
                     <div>
                         <h2>Feed Back Form</h2>
@@ -35,10 +46,11 @@ function FormInput(){
                         <input type="range" max="10" min="0" value={score} onChange={ e => setScore(e.target.value)} />
                         
                     </div>
-                    <div className="comment">
+                    <div>
                         <label>
                             Comment:
                         </label>
+                        <textarea value={comment} onChange={e => setComment(e.target.value)}/>
                     </div>
                     <button type="submit" >Submit</button>
                 </fieldset>
